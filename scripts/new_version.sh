e68dd7eb657b602378b0dc1d4ecacd9a69a71c83
Tomasz Lemkowski <lemkowski@gmail.com>
2019-05-15 13:53:51 +0200

    release version script

--- scripts/new_version.sh
+++ scripts/new_version.sh
@@ -20,7 +20,8 @@ echo "Current version is $current_version"
 next_version=$(echo $current_version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
 
 echo "Preparing zip file for version $next_version"
-zip -rq "tmp/$next_version.zip" .  -x scrtipts -x tmp
+
+git archive HEAD --prefix=payments/  --format=zip -o "tmp/$next_version.zip"
 
 confirm "Would you like to create new release tag? $next_version" && git tag $next_version
 confirm "Would you like to push local tags to remote repository?" && git push --tags
